bea5380927cdd2404e55c46c6e74a4a0968c5df1
Niccolò Gomiero <niccolo.gomiero@edu.iisleviponti>
2026-03-19 22:52:54 +0100

    Incapsulamento dinamico per gestione

---
+++ src/main/java/com/gomiero/progettonegomiero/controllers/PersonalNotesController.java
@@ -0,0 +1,11 @@
+package com.gomiero.progettonegomiero.controllers;
+
+import com.gomiero.progettonegomiero.models.Utente;
+
+public class PersonalNotesController {
+    private Utente utente;
+    public void setUtente(Utente utente) {
+        this.utente = utente;
+    }
+
+}
---
+++ src/main/java/com/gomiero/progettonegomiero/controllers/TutorialNotesController.java
@@ -0,0 +1,16 @@
+package com.gomiero.progettonegomiero.controllers;
+
+import com.gomiero.progettonegomiero.models.Utente;
+
+public class TutorialNotesController  {
+
+    private Utente utente;
+
+    public void setUtente(Utente utente) {
+        this.utente = utente;
+    }
+
+
+
+
+}
