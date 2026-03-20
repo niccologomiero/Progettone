0124e082b3da39c718ad7923fa0e4a1df9cbe6ff
Niccolò Gomiero <niccolo.gomiero@edu.iisleviponti>
2026-03-20 21:48:44 +0100

    Ottimizzazione del cambio scena con metodo dedicato, uso di classe tipo interface implements per gestione dinamica

--- src/main/java/com/gomiero/progettonegomiero/controllers/HomeController.java
+++ src/main/java/com/gomiero/progettonegomiero/controllers/HomeController.java
@@ -10,31 +10,44 @@ import javafx.scene.Parent;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
 import java.text.DateFormatSymbols;
 import java.util.Arrays;
 import java.util.Locale;
+import java.util.Objects;
 
-public class HomeController {
+public class HomeController implements SetterUtente {
     private Utente utente;
     private String tempNickName;
 
     @FXML
     private Label titlePortfolio;
     @FXML
+    public Button btn_GoLogout;
+    @FXML
+    public Button btn_GoGraph;
+    @FXML
+    public Button btn_GoNotes;
+    @FXML
     private BarChart<String,Integer> barChart;
     @FXML
     private CategoryAxis asseX;
     @FXML
     private VBox containerInit;
-    private ObservableList<String> mesiNomi = FXCollections.observableArrayList();
+    private final ObservableList<String> mesiNomi = FXCollections.observableArrayList();
 
-    public void setterUtente(Utente t, String nickName) {
+    public void setterUtente(Utente t) {
         this.utente = t;
+
+    }
+    public void setTempNickName(String nickName){
         this.tempNickName = nickName;
         if (titlePortfolio != null){
             titlePortfolio.setText("Portfolio di " + nickName);
@@ -65,7 +78,7 @@ public class HomeController {
         }
         barChart.getData().add(serie);
     }
-        //TODO:esportare controller e l'utente come in notes
+        //FIXME:esportare controller e l'utente come in notes
     public void goToNotes(ActionEvent actionEvent) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(
@@ -83,7 +96,8 @@ public class HomeController {
             throw new RuntimeException(e);
         }
     }
-
+    //deprecated: questi metodi avranno un unico
+    // metodo che riconosce che tipo di btn è cliccato
     public void ReturnPage(ActionEvent actionEvent) {
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(
@@ -98,6 +112,54 @@ public class HomeController {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+    public void changePage(ActionEvent actionEvent){
+        Object sourceBtn = actionEvent.getSource();
+        String destinazione = "";
+         if(sourceBtn == btn_GoLogout){
+                destinazione = "quit";
+         }else if (sourceBtn == btn_GoNotes){
+             destinazione = "notes";
+        } else if (sourceBtn == btn_GoGraph) {
+             destinazione = "graph";
+         }
+
+        String fxmlPath = pathPage(destinazione);
+        try{
+            URL resource = getClass().getResource(fxmlPath);
+            if (resource == null){
+                throw new FileNotFoundException("Impossibile trovare il file FXML: " + fxmlPath);
+            }
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+
+            Parent root = loader.load();
+
+            Controller controller = loader.getController();
+
+            Stage stage = (Stage) containerInit.getScene().getWindow();
+            stage.getScene().setRoot(root);
+
+            if (controller instanceof SetterUtente) ((SetterUtente) controller).setterUtente(utente);
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }catch (Exception e){
+            System.err.println("Errore critico durante il cambio pagina" + e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+    private String pathPage(String destinazione){
+        String path = switch (destinazione) {
+            case "quit" -> "/com/gomiero/progettonegomiero/views/notes-plugin/login-views.fxml";
+            case "note" -> "/com/gomiero/progettonegomiero/views/notes-plugin/notes-views.fxml";
+            case "graph" -> ""; //todo ancora da indirizzare
+            default -> "";
+        };
+        if (path.isEmpty()){
+            path = "/com/gomiero/progettonegomiero/views/notes-plugin/home-view.fxml";
+        }
+        return path;
     }
     //TODO: fai btn per graph page;
 }
---
+++ src/main/java/com/gomiero/progettonegomiero/controllers/SetterUtente.java
@@ -0,0 +1,7 @@
+package com.gomiero.progettonegomiero.controllers;
+
+import com.gomiero.progettonegomiero.models.Utente;
+
+public interface SetterUtente {
+    void setterUtente(Utente t);
+}
